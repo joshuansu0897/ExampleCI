d913c4d9078bfb6ec87155c3968fb6e54288b709
Joshua Saucedo <joshuansu0897@gmail.com>
2018-03-26 17:28:02 -0700

    agregando el lb

--- infra/main.tf
+++ infra/main.tf
@@ -42,3 +42,29 @@ coreos:
           ExecStart=/usr/bin/docker run -d -p 3000:3000 devops-demo
 EOF
 }
+
+# LoadBalancer
+resource "digitalocean_loadbalancer" "demo" {
+  name   = "lb-DesdeTerraform"
+  region = "sfo3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 3000
+    protocol = "http"
+  }
+
+  # esto es para agregar por el id del droplet
+  #droplet_ids = ["${digitalocean_droplet.demo.id}"]
+
+  # esto es para agregar por el tag
+  #droplet_tag = "DesdeTerraform" mejor el de abajo por si cambia
+  droplet_tag = "${digitalocean_tag.demo.name}"
+}
