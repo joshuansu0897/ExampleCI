95bac56d0fab3b2c263d817bffa6d8a03ee0830e
Joshua Saucedo <joshuansu0897@gmail.com>
2018-04-10 09:36:32 -0700

    en teoria el flujo completo ya quedo

--- deployment/prod.sh
+++ deployment/prod.sh
@@ -1,4 +1,58 @@
-wget -O packer.zip https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_linux_amd64.zip
-unzip packer.zip
-./packer validate deployment/template.json
-./packer build deployment/template.json
+# nada mas para que se vea mejor y para saber que esta pasando
+echo "**********************************"
+echo "* Descargamos Packer Y Terraform *"
+echo "**********************************"
+
+# descargamos packer en /tmp/packer.zip
+wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_linux_amd64.zip
+# descargamos terraform en /tmp/terraform.zip
+wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.6/terraform_0.11.6_linux_amd64.zip
+
+# nada mas para que se vea mejor y para saber que esta pasando
+echo "***************************************"
+echo "* Descomprecion de Packer Y Terraform *"
+echo "***************************************"
+
+# descomprimimos el /tmp/packer.zip en /bin
+unzip /tmp/packer.zip -d ~/bin
+# descomprimimos el /tmp/terraform.zip en /bin
+unzip /tmp/terraform.zip -d ~/bin
+
+# nada mas para que se vea mejor y para saber que esta pasando
+echo "*****************************************"
+echo "* Validamos y Buldeamos template packer *"
+echo "*****************************************"
+
+# validamos el template para apcker
+packer validate deployment/template.json &&
+# buldeamos el template
+packer build deployment/template.json &&
+
+# guardamos el "ID" de la imagen recien buldeada y se guarda en la variable "TF_VAR_image_id" esta variable es necesaria porque terraform la tomara para saber que imagen usar para los droplets
+export TF_VAR_image_id=$(curl -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" "https://api.digitalocean.com/v2/images?&private=true" | jq ."images[] | select(.name == \"snapshot-devops-template-$CIRCLE_BUILD_NUM\") | .id")
+
+# nada mas para que se vea mejor y para saber que esta pasando
+echo ""
+echo "el ID de snapshot-devops-template-$CIRCLE_BUILD_NUM es: $TF_VAR_image_id"
+echo ""
+
+# nada mas para que se vea mejor y para saber que esta pasando
+echo "**************************************************"
+echo "* Validamos y Aplicamos Infrastructura Terraform *"
+echo "**************************************************"
+
+# entramos a la carpeta
+cd infra && 
+# aplicamos el "-input=false" para, literal hace lo que dice... y el "-auto-approve" es para que no pida confirmacion
+terraform apply -input=false -auto-approve &&
+# regresamos a la raiz
+cd ..
+
+# nada mas para que se vea mejor y para saber que esta pasando
+echo "*************************"
+echo "* Commit de los cambios *"
+echo "*************************"
+
+git add infra && 
+git commit -m "Deployed snapshot-devops-template-$CIRCLE_BUILD_NUM [skip ci]" &&
+git push origin master
