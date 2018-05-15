6819138bffab09b8f2603d4e0df338fba0e844a9
Joshua Saucedo <joshuansu0897@gmail.com>
2018-05-15 15:45:34 -0700

    creo que ya

--- infra/backend.tf
+++ infra/backend.tf
@@ -12,6 +12,8 @@ terraform {
     skip_credentials_validation = true
     skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
+    access_key                  = "${var.access_key}"
+    secret_key                  = "${var.secret_key}"
     endpoint                    = "https://nyc3.digitaloceanspaces.com"
     region                      = "us-east-1"
     bucket                      = "terraform-backend-joshua-test"
