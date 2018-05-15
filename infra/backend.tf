a5bb99fbfd8b2449d6a1226215820a6927aab67b
Joshua Saucedo <joshuansu0897@gmail.com>
2018-05-15 15:11:48 -0700

    detalles

--- infra/backend.tf
+++ infra/backend.tf
@@ -7,13 +7,13 @@
 # }
 
 terraform {
-  required_version = ">= 0.11, < 0.12"
-
   backend "s3" {
     skip_requesting_account_id  = true
     skip_credentials_validation = true
     skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
+    access_key                  = "${var.access_key}"
+    secret_key                  = "${var.secret_key}"
     endpoint                    = "https://nyc3.digitaloceanspaces.com"
     region                      = "us-east-1"
     bucket                      = "terraform-backend-joshua-test"
